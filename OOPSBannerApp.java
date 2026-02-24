632a2c517b04c739d99491fc5f00860a2df6d115
Ananya Gupta <guptaananya.9258@gmail.com>
2026-02-24 13:17:50 +0530

    UC4: print OOPS

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,19 @@
-public class OOPSBannerApp{
-public static void main(String[] args){
-	System.out.println("OOPS");
-}
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        String[] lines = new String[7];
+
+        lines[0] = " *****   *****   *****   *****  ";
+        lines[1] = "*     * *     * *     * *     * ";
+        lines[2] = "*     * *     * *     * *       ";
+        lines[3] = "*     * *     * *****   *****   ";
+        lines[4] = "*     * *     * *            *  ";
+        lines[5] = "*     * *     * *            *  ";
+        lines[6] = " *****   *****   *       *****   ";
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
 }

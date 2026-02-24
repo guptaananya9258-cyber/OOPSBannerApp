0992f0da22e587996eadacf8fa837a1f569281ad
Ananya Gupta <guptaananya.9258@gmail.com>
2026-02-24 06:59:05 +0530

    Initial commit: add printOOPS Java file

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,24 +1,5 @@
 public class OOPSBannerApp{
 public static void main(String[] args){
-	System.out.println(String.join("",
-                " *****   ", " *****   ", " *****   ", " *****  "));
-        
-    System.out.println(String.join("",
-                "*     *  ", "*     *  ", "*     *  ", "*     * "));
-        
-    System.out.println(String.join("",
-                "*     *  ", "*     *  ", "*     *  ", "*       "));
-        
-    System.out.println(String.join("",
-                "*     *  ", "*     *  ", " *****   ", " *****   "));
-        
-    System.out.println(String.join("",
-                "*     *  ", "*     *  ", "*        ", "     *   "));
-        
-    System.out.println(String.join("",
-                "*     *  ", "*     *  ", "*        ", "     *   "));
-        
-    System.out.println(String.join("",
-                " *****   ", " *****   ", "*        ", " *****   "));
+	System.out.println("OOPS");
 }
 }
